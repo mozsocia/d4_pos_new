040626c322373b280dfe00604702bf167f5b3e3f
Mozdalif sikder <mozsocia@gmail.com>
2023-08-31 01:23:56 +0600

    Revert "Revert "gitignore add""
    This reverts commit d21534111c6e9c0888a53cc9eb98d61fa8985537.

--- _core/urls.py
+++ _core/urls.py
@@ -19,6 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('dashboard.urls')),
     path('pos/', include('pos_dashboard.urls')),
 ]
